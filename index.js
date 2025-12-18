9123afa59100d8adbf0f2f7153208c1a1aea38e8
Denis Coello <denis.coello.7e8@itb.cat>
2025-12-18 10:05:52 +0100

    session

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import session from "express-session";
 import dotenv from 'dotenv';
+import pgSession from 'connect-pg-simple';
+import pg from 'pg';
 import { errorHandler } from './middlewares/error.js';
 import loginRouter from "./routes/auth.js";
 import usersRouter from "./routes/users.js";
@@ -14,7 +16,15 @@ import typologiesRouter from "./routes/typologies.js";
 
 dotenv.config();
 const PORT = 3000;
+
+const PgSession = pgSession(session);
+const pgPool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
 const app = express();
+
+app.set('trust proxy', 1);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,12 +34,20 @@ app.set('view engine', 'ejs');
 
 // Configuración de la sesión 
 app.use(session({
+    store: new PgSession({
+        pool: pgPool,
+        tableName: 'session',
+        createTableIfMissing: true
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true
+    }
 }));
-
 // Middleware para pasar el usuario a las vistas
 app.use((req, res, next) => {
     res.locals.user = req.session.user;
